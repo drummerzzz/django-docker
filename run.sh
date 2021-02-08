d1cf0523be2efc638228cec10b5589eaeb7d2111
Drummerzzz <jf.interatividade@gmail.com>
2021-02-08 09:50:01 -0300

    :art: update run and start sh

--- run.sh
+++ run.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
-# python manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
+
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+pip install -r dev_requirements.txt
+
+python manage.py makemigrations
 python manage.py migrate
+
 python manage.py runserver 0.0.0.0:8000
--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Diretorio e arquivo de log
 set -e
-LOGFILE=/home/ubuntu/viralize/logs/gunicorn.log
+LOGFILE=/home/ubuntu/app/logs/gunicorn.log
 LOGDIR=$(dirname $LOGFILE)
 # Numero de processo simultaneo, modifique de acordo com seu processador
 NUM_WORKERS=1
@@ -12,10 +12,10 @@ USER=ubuntu
 ADDRESS=127.0.0.1:8000
 # Ativando ambiente virtual e executando o gunicorn para este projeto
 source /home/ubuntu/env/bin/activate
-cd /home/ubuntu/viralize/
+cd /home/ubuntu/app/
 # Migrate dos models
 python manage.py makemigrations;
 python manage.py migrate;
 #Executa o gunicorn
 test -d $LOGDIR || mkdir -p $LOGDIR
-exec gunicorn viralize.wsgi:application
+exec gunicorn server.wsgi:application
